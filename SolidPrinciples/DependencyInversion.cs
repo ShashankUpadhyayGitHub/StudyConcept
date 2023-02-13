79dfd34e4f55ff3d9344598b6fe3c6fdf29a5658
Shashank Upadhyay <shashankupadhyay96@gmail.com>
2023-02-14 00:21:27 +0530

    Solid Principles - Dependency Inversion

---
+++ SolidPrinciples/DependencyInversion.cs
@@ -0,0 +1,30 @@
+﻿using BusinessObject;
+
+namespace SolidPrinciples
+{
+    public class DependencyInversion
+    { }
+
+    //Add intermediate class "PersonFactory" to call Person class
+    public class PersonFactory
+    {
+        public static Person GetPersonInfo()
+        {
+            return new Person();
+        }
+    }
+
+    //Access the Person without create the object of Person class using loosely coupled
+
+    public class Company
+    {
+        public Company()
+        {
+        }
+        public string GetPersonName()
+        {
+            Person objPerson = PersonFactory.GetPersonInfo();
+            return objPerson.Name;
+        }
+    }
+}
